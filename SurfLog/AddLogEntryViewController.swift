5d1f5859126ef8e8e16c0e20ba220e25339f8ae7
Alex Brashear <alexjbrashear@gmail.com>
2015-10-24 20:29:04 -0400

    AddLogEntryViewController

---
+++ SurfLog/AddLogEntryViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddLogEntryViewController.swift
+//  SurfLog
+//
+//  Created by Alex Brashear on 10/24/15.
+//  Copyright © 2015 Alex Brashear. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AddLogEntryViewController: UIViewController {
+    
+    var managedContext: NSManagedObjectContext!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
